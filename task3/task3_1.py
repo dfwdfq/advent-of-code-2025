ffb5306eda46d08eae98a04eedca04c34c3aec0d
uncle_joe <uncle_joe@kali.kali.com>
2025-12-03 10:51:37 +0300

    solved first part

---
+++ task3/task3_1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+with open("input","r") as f:
+    data = f.read().strip().split("\n")
+
+def remove_duplicates(lst):
+    out = []
+    for el in lst:
+        if el not in out:
+            out.append(el)
+    return out
+def listify_s(s):
+    return [int(x) for x in list(s)]
+def cat(s):
+    return [str(x) for x in s]
+def find_max(lst):    
+    m1,m2 = max(lst), None
+    pos = lst.index(m1)
+    #search backward
+    if pos == len(lst)-1:
+        del(lst[-1])
+        lst = lst[::-1]
+        m2 = max(lst)
+        return int(f"{m2}{m1}")
+    else:#search forward
+        lst = lst[pos+1:]
+        m2 = max(lst)
+        return int(f"{m1}{m2}")            
+
+S = 0
+for v in data:
+    S+=find_max(listify_s(v))
+
+print(S)
